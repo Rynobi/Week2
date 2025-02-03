387be70e805111475a1f6c07054cac0064becf60
Rynobi <ryanlok@ocadu.ca>
2025-02-03 09:19:39 -0500

    new

--- sketch.js
+++ sketch.js
@@ -1,12 +1,17 @@
-
+var x = 0;
+var y = 0;
 
 function setup(){
     createCanvas(720,480);
-
 }
 
 function draw(){
     background('#4287f5');
     fill("blue");
+    rect(x,y,10,10);
+    x = x + 1;
+    x = x % 500;
+    y = y + 2;
+    y = y % 400;
     ellipse(width/2, height/2, 100, 100);
 }
