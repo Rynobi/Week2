8f0c2093b998ed916912a813e293c1577bbaa38d
Rynobi <ryanlok@ocadu.ca>
2025-02-03 09:04:05 -0500

    First Ver

---
+++ script.js
@@ -0,0 +1,38 @@
+0; // Hello
+1234567;
+12345.67;
+
+Infinity;
+
+var SomethingMemorable;
+
+SomethingMemorable = 15
+
+var SomethingMoreMemorable = 30; //Hello
+
+console.log(SomethingMemorable);
+
+SomethingMemorable = SomethingMoreMemorable;
+
+console.log(SomethingMemorable);
+
+var SomethingLessMemorable;
+console.log(SomethingLessMemorable);
+
+var SomeString = "Something Inside Here";
+var SomeSingleString = 'Something Inside Here Aswell';
+
+var S = true;
+x = false;
+
+var ball = {};
+ball = null;
+
+1 + 2;
+SomethingMemorable = 3 + 4;
+var SomethingMemorable;
+
+SomethingMemorable = 4 + "4";
+console.log(SomethingMemorable);
+
+
---
+++ sketch.js
@@ -0,0 +1,12 @@
+
+
+function setup(){
+    createCanvas(720,480);
+
+}
+
+function draw(){
+    background('#4287f5');
+    fill("blue");
+    ellipse(width/2, height/2, 100, 100);
+}
